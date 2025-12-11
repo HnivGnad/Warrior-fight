a446231a8d4cee7275968d489685cb27a5db60b5
vinh-pc <vjnhdang03@gmail.com>
2025-12-11 22:27:34 +0700

    loopBackground

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,22 +4,38 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
+    private float imageWidthOffset = 10f;
 
     [SerializeField] private ParallaxLayer[] backgroundLayer;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        InitializeLayer();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         float currentCamaraPositionX = mainCamera.transform.position.x;
         float distanceToMove = currentCamaraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCamaraPositionX;
 
+        float cameraLeftEdge = (currentCamaraPositionX - cameraHalfWidth) + imageWidthOffset;
+        float cameraRightEdge = (currentCamaraPositionX + cameraHalfWidth) - imageWidthOffset;
+
         foreach (ParallaxLayer layer in backgroundLayer)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackGround(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+
+    private void InitializeLayer()
+    {
+        foreach (ParallaxLayer layer in backgroundLayer)
+        {
+            layer.CalculateImageWith();
         }
     }
 }
--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -5,8 +5,30 @@ public class ParallaxLayer
     [SerializeField] private Transform background;
     [SerializeField] private float parallaxMutiplier;
 
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+    public void CalculateImageWith()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
     public void Move(float distanceToMove)
     {
         background.position += Vector3.right * (distanceToMove * parallaxMutiplier);
+    }
+    public void LoopBackGround(float cameraLeftEdge, float cameraRightEdge)
+    {
+        float imageLeftEdge = background.position.x - imageHalfWidth;
+        float imageRightEdge = background.position.x + imageHalfWidth;
+
+        if(imageRightEdge < cameraLeftEdge)
+        {
+            background.position += Vector3.right * imageFullWidth;
+        }
+        else if(imageLeftEdge > cameraRightEdge)
+        {
+            background.position += Vector3.right * -imageFullWidth;
+        }
     }
 }
