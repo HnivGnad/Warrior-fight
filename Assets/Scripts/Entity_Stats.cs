9ba2a681d7afa1d1e9bebde641ff18b59146978d
Vinh Đặng <vjnhdang03@gmail.com>
2026-07-07 16:36:55 +0700

    EntityState

--- Assets/Scripts/Entity_Stats.cs
+++ Assets/Scripts/Entity_Stats.cs
@@ -6,6 +6,28 @@ public class Entity_Stats : MonoBehaviour
     public Stat_MajorGroup major;
     public Stat_OffenseGroup offense;
     public Stat_DefenseGroup defense;
+
+
+    public float GetPhysicalDamage()
+    {
+        float baseDamage = offense.damage.GetValue();
+        float bonusDamage = major.strength.GetValue();
+        float totalBaseDamage = baseDamage + bonusDamage;
+
+        float baseCritChance = offense.critChance.GetValue();
+        float bonusCritChance = major.agiliti.GetValue() * .3f; //Chance from agility: +0.3% per AGI
+        float critChance = baseCritChance + bonusCritChance;
+
+        float baseCritPower = offense.critPower.GetValue();
+        float bonusCritPower = major.strength.GetValue(); //Chance from Strength: +0.3% per AGI
+        float critPower = (baseCritPower + bonusCritPower) / 100; //Total crit power as multiplier 
+
+        bool isCrit = Random.Range(0, 100) < critChance;
+        float finalDamage = isCrit ? totalBaseDamage * critPower : totalBaseDamage;
+
+        return finalDamage;
+
+    }
     public float GetMaxHealth()
     {
         float baseHp = maxHealth.GetValue();
